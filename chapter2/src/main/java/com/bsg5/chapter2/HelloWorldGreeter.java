3198d6db68e84c17cc50fa6c562df22a9c72ee1e
Oleksiy Blavat <oleksiy.blavat@verizonmedia.com>
2019-10-04 23:44:24 -0400

    initial commit

---
+++ chapter2/src/main/java/com/bsg5/chapter2/HelloWorldGreeter.java
@@ -0,0 +1,15 @@
+package com.bsg5.chapter2;
+
+import org.springframework.stereotype.Service;
+import java.io.PrintStream;
+
+public class HelloWorldGreeter implements Greeter {
+	private PrintStream printStream = System.out;
+
+	public void setPrintStream(PrintStream printStream) {
+		this.printStream = printStream;
+	}
+	public void greet() {
+		printStream.print("Hello, World!");
+	}
+}
