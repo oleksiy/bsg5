bce0416ec3e2ca668a555fce313bed52ce1a016e
Oleksiy Blavat <blavat.alex@gmail.com>
2019-12-26 15:58:08 -0500

    Additional lifecycle tests and examples added

---
+++ chapter4/src/test/java/com/bsg5/chapter4/TestLifeCycle03.java
@@ -0,0 +1,38 @@
+package com.bsg5.chapter4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+
+class ThirdObject extends HasData {
+    static Object semaphore = null;
+
+    public void init() {
+        semaphore = new Object();
+    }
+
+    public void dispose() {
+        semaphore = null;
+    }
+}
+
+@ContextConfiguration(locations = "/config-03.xml")
+public class TestLifeCycle03 extends AbstractTestNGSpringContextTests {
+    @Autowired
+    ConfigurableApplicationContext context;
+
+    @Test
+    public void testInitDestroyMethods() {
+        ThirdObject o1 = context.getBean(ThirdObject.class);
+        assertNotNull(ThirdObject.semaphore);
+        assertEquals(o1.getDatum(), "default");
+        context.close();
+        assertNull(ThirdObject.semaphore);
+    }
+}
---
+++ chapter4/src/test/java/com/bsg5/chapter4/TestLifecycle07.java
@@ -0,0 +1,60 @@
+package com.bsg5.chapter4;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import javax.swing.tree.FixedHeightLayoutCache;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+
+@Component
+class EigthObject extends HasData implements InitializingBean, DisposableBean {
+    static Object semaphore = null;
+
+    /**
+     * Invoked by the containing {@code BeanFactory} on destruction of a bean.
+     *
+     * @throws Exception in case of shutdown errors. Exceptions will get logged
+     *                   but not rethrown to allow other beans to release their resources as well.
+     */
+    @Override
+    public void destroy() throws Exception {
+        semaphore = null;
+    }
+
+    /**
+     * Invoked by the containing {@code BeanFactory} after it has set all bean properties
+     * and satisfied {@link BeanFactoryAware}, {@code ApplicationContextAware} etc.
+     * <p>This method allows the bean instance to perform validation of its overall
+     * configuration and final initialization when all bean properties have been set.
+     *
+     * @throws Exception in the event of misconfiguration (such as failure to set an
+     *                   essential property) or if initialization fails for any other reason
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        semaphore = new Object();
+    }
+}
+@ContextConfiguration(locations = "/annotated-07.xml")
+public class TestLifecycle07 extends AbstractTestNGSpringContextTests {
+    @Autowired
+    ConfigurableApplicationContext context;
+
+    @Test
+    public void testLifecycleMethods() {
+        EigthObject o1 = context.getBean(EigthObject.class);
+        assertNotNull(EigthObject.semaphore);
+        assertEquals(o1.getDatum(), "default");
+        context.close();
+        assertNull(EigthObject.semaphore);
+    }
+}
